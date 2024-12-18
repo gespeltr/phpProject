ef2c81b12ac0f09121f1fabf76c554ffa85cbaf9
gespeltr <guillemespeltr@gmail.com>
2024-12-19 00:18:33 +0100

    add isset()

--- public/afegir_web.php
+++ public/afegir_web.php
@@ -1,9 +1,10 @@
 <?php
     session_start();
 
-    if (!$_SESSION['login']){
+    if (!isset($_SESSION['login'])){
         header("Location: index.php");
     }
+
     require_once '../db/conn.php';
     include 'header.php';
     include 'footer.php';
--- public/modificar_dades.php
+++ public/modificar_dades.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
 
-    if (!$_SESSION['login']){
+    if (!isset($_SESSION['login'])){
         header("Location: index.php");
     }
     
